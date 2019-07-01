dc05e01a57f78cc0dcce122df41b39b6e979a3d0
Hwan Dong <hwandong@yahoo.com>
2019-07-01 15:52:26 -0700

    Started to make changes.

--- GlacierServices-v1/src/main/java/CreateVault.java
+++ GlacierServices-v1/src/main/java/CreateVault.java
@@ -2,12 +2,37 @@ import com.amazonaws.services.glacier.AmazonGlacier;
 import com.amazonaws.services.glacier.AmazonGlacierClientBuilder;
 import com.amazonaws.services.glacier.model.CreateVaultRequest;
 import com.amazonaws.services.glacier.model.CreateVaultResult;
+import org.apache.commons.cli.*;
 
 public class CreateVault {
     public static void main(String[] args) {
 
-        String accountId;
-        String vaultName;
+        Options options = new Options();
+
+        Option account = new Option("accountId", "account id", true, "account id");
+        account.setRequired(true);
+        options.addOption(account);
+
+        Option vault = new Option("vaultName", "vault name ", true, "vault name");
+        vault.setRequired(true);
+        options.addOption(vault);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd = null;
+
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("utility-name", options);
+
+            System.exit(1);
+        }
+
+        String accountId = cmd.getOptionValue("accountId");
+        String vaultName = cmd.getOptionValue("vaultName");
+
         CreateVaultRequest request = null;
 
         AmazonGlacier glacierClient = AmazonGlacierClientBuilder.defaultClient();
