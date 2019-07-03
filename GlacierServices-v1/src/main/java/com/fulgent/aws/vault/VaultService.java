0cf97029236337bd99bbc8d0a319d96b6a6fff42
ntsliyang <yli546@usc.edu>
2019-07-03 16:52:45 -0700

    add downloadVault

--- GlacierServices-v1/src/main/java/com/fulgent/aws/vault/VaultService.java
+++ GlacierServices-v1/src/main/java/com/fulgent/aws/vault/VaultService.java
@@ -3,6 +3,7 @@ package com.fulgent.aws.vault;
 import com.amazonaws.services.glacier.AmazonGlacier;
 import com.amazonaws.services.glacier.model.DescribeVaultOutput;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface VaultService {
@@ -13,4 +14,18 @@ public interface VaultService {
     String createVault(String vaultName); // return location of newly created vault
 
     void deleteVault(String vaultName);
+
+    void downloadVault(String vaultName, String fileName);
+
+    void downloadJobOutput(String jobId, String vaultName) throws IOException;
+
+    void setupSNS();
+
+    void setupSQS();
+
+    String initiateJobRequest(String vaultName);
+
+    Boolean waitForJobToComplete(String jobId, String sqsQueueUrl) throws InterruptedException, IOException;
+
+    void cleanUp();
 }
